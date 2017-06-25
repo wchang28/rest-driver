94b5d167fe74c970b70272b350568c4661d19ee8
wchang28 <wchang28@hotmail.com>
2017-06-25 11:22:10 -0400

    first commit

---
+++ index.d.ts
@@ -0,0 +1,46 @@
+// driver interfaces
+import * as restIntf from 'rest-api-interfaces';
+import * as eventSource from 'eventsource-typings';
+
+export * from 'rest-api-interfaces';
+export {EventSourceConstructor, IEventSource} from 'eventsource-typings';
+
+export interface I$EReturn {
+    eventSrc: eventSource.IEventSource; // event source object
+    initMsgs?: eventSource.Message[];   // initial messages rcvd. before the "OPEN" event. This could happen when using EventSource polyfill in browser that don't support EventSource natively (IE)
+}
+
+export type IReadableBlob = any;
+export type IFormData = any;
+
+// JSON in and JSON out
+export type I$J = (method: restIntf.HTTPMethod, url:string, data:any, options?: restIntf.ApiCallOptions) => Promise<restIntf.RESTReturn>;
+
+// EventSource factory
+export type I$E = (url:string, options?: restIntf.ApiCallOptions) => Promise<I$EReturn>;
+
+// send FormData
+export type I$F = (method: restIntf.HTTPMethod, url:string, formData: IFormData, options?: restIntf.ApiCallOptions) => Promise<restIntf.RESTReturn>;
+
+// HEAD call on object
+export type I$H = (url:string, qs?:any, options?: restIntf.ApiCallOptions) => Promise<restIntf.RESTReturn>;
+
+// download file blob
+export type I$B = (url:string, qs?:any, options?: restIntf.ApiCallOptions) => Promise<restIntf.RESTReturn>;
+
+// upload file blob
+export type I$U = (method: restIntf.HTTPMethod, url:string, contentInfo: restIntf.ContentInfo, blob: IReadableBlob, options?: restIntf.ApiCallOptions) => Promise<restIntf.RESTReturn>;
+
+// FormData factory
+export type IFormDataFactory = () => IFormData;
+
+// the driver interface
+export interface $Driver {
+    $J: I$J;
+    $E: I$E;
+    $F: I$F;
+    $H: I$H;
+    $B: I$B;
+    $U: I$U;
+    createFormData: IFormDataFactory;
+}
